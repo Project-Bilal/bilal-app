f463ca81862c41c3aa0687ae08db34815f869de9
Omar Sarhan <omars@zillowgroup.com>
2023-11-10 09:40:09 -0800

    add secrets

---
+++ app.config.js
@@ -0,0 +1,42 @@
+export default {
+  expo: {
+    name: "bilal-app",
+    slug: "bilal-companion-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.projectbilal.app",
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription: "Location to determine prayer times.",
+        NSLocationAlwaysAndWhenInUseUsageDescription: "Location to determine prayer times.",
+        NSLocationAlwaysUsageDescription: "Location to determine prayer times.",
+        NSAllowsArbitraryLoads: true,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.projectbilal.app",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "5efe247a-dcb9-4a1a-86a6-933e5d72cdcb",
+      },
+    },
+    owner: "projectbilal",
+  },
+};
